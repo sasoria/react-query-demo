eb820c7a3e2af104681f6b69c842ba14d233a092
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-17 22:56:13 +0100

    Remove unused code

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,7 +11,7 @@ const replaceLuke = (obiWan, mutation) => {
 function Button() {
   const { data: obiWan } = useQuery(obiWanUrl, fetcher);
   const { data: luke } = useQuery(lukeUrl, fetcher);
-  const mutation = useMutatePerson(obiWan?.name);
+  const mutation = useMutatePerson();
 
   return (
     <button onClick={() => replaceLuke(obiWan, mutation)}>Bye {luke?.name}</button>
